4efc44b1b4e21a6b4b080b5b4c4752fc0ca7801f
Moatmas <alain.abhay@epfedu.fr>
2024-12-18 23:03:01 +0100

    Ajout notice et menu + lvl 1 tuto

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -8,11 +8,10 @@ using TMPro;
 public class Events : MonoBehaviour
 {
     public GameObject ExitPanel;
-    public Button[] levelButtons;  // Boutons des niveaux
-    public TextMeshProUGUI selectedLevelText; // Affichage texte du niveau sélectionné
+    public Button[] levelButtons;  
+    public TextMeshProUGUI selectedLevelText; 
     private int selectedLevel = -1;
 
-    // Sélectionne le niveau choisi par le joueur
     public void SelectLevel(int levelNumber)
     {
         selectedLevel = levelNumber;
@@ -21,7 +20,6 @@ public class Events : MonoBehaviour
 
 
 
-    // Charge la scène correspondant au niveau choisi
     public void PlaySelectedLevel()
     {
         if (selectedLevel >= 0 && selectedLevel <= 4)
@@ -34,30 +32,28 @@ public class Events : MonoBehaviour
         }
     }
 
-    // Affiche un message de notice (à personnaliser selon le besoin)
-    public void ShowNotice()
+    public void Notice()
     {
         SceneManager.LoadScene("Notice");
-        // Ajouter la logique pour afficher une notice UI si nécessaire
     }
     public void MenuPreGame()
     {
         SceneManager.LoadScene("MenuPreGame");
-        // Ajouter la logique pour afficher une notice UI si nécessaire
     }
     public void Menu()
     {
         SceneManager.LoadScene("Menu");
-        // Ajouter la logique pour afficher une notice UI si nécessaire
+    }
+    public void Notice2()
+    {
+        SceneManager.LoadScene("Notice2");
     }
 
-    // Quitte le jeu
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    // Active le panneau de sortie et met le jeu en pause
     public void OpenLevelExitPanel()
     {
         if (ExitPanel != null)
